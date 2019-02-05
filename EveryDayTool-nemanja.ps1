a4e1b8bd38e739f9db7ad9b402e182715c0bf1d4
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-02-05 15:21:20 +0100

    Adding Nemanjas duplicate

---
+++ EveryDayTool-nemanja.ps1
@@ -0,0 +1,10 @@
+<#
+
+.SYNOPSIS
+
+.DESCRIPTION
+EveryDay Powershell automation tool represents open source collaboration project with the goal to speed up the day-to-day administration tasks in Microsoft environments.
+In the essence, it is just a bundle of functions that will be executed based on user input choice.
+
+
+#>
