4e31efc2e8c8952ec4ca667240d0b949db4e3b4d
Nemanja Jovic <jnemanja.info@gmail.com>
2019-02-18 13:37:22 +0100

    Update EveryDayTool-nemanja.ps1
    adding a comment

--- EveryDayTool-nemanja.ps1
+++ EveryDayTool-nemanja.ps1
@@ -1,5 +1,7 @@
 <#
 
+just a comment
+
 .DESCRIPTION
 Powershell4All automation tool represents an open source collaboration project with the goal to speed up the day-to-day administration tasks in Microsoft environments.
 In the essence, it is just a bundle of functions that will be executed based on user input choice.
